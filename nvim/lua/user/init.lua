22f86d7f60b0b1baac22e452718fdfc8031784d1
Parker <aparkerdf@gmail.com>
2023-08-13 13:51:50 -0400

    Hyprland

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -1,45 +1,59 @@
 return {
-  colorscheme = "tokyonight-night",
-  lsp = {
-    -- skip_setup = { "rust_analyzer" }, -- rust-tools will handle setting up the LSP
+  colorscheme = "tokyonight",
+  -- the init key will apply to all colorschemes, this sets transparency
+  highlights = {
+    -- apply highlight group to all colorschemes (include the default_theme)
+    init = {
+      -- set the transparency for all of these highlight groups
+      Normal = { bg = "NONE", ctermbg = "NONE" },
+      NormalNC = { bg = "NONE", ctermbg = "NONE" },
+      CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      LineNr = {},
+      SignColumn = {},
+      StatusLine = {},
+      NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+      NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+    },
   },
-  -- maps.n["]b"] = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
+
 
   plugins = {
+
+    "AstroNvim/astrocommunity",
     {
       "folke/tokyonight.nvim",
-      name = "tokyonight"
-    }, 
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- Add gigachad RUST tools
-  { import = "astrocommunity.pack.rust", enabled = true },
-  { "rust-tools.nvim", opts = {
-      tools = { -- rust-tools options
-      reload_workspace_from_cargo_toml = true,
-      inlay_hints = {
-        auto = true,
-        only_current_line = false,
-        show_parameter_hints = true,
-      },
-      hover_actions = {
-        border = {
-          { "╒", "FloatBorder" },
-          { "═", "FloatBorder" },
-          { "╕", "FloatBorder" },
-          { "│", "FloatBorder" },
-          { "╛", "FloatBorder" },
-          { "─", "FloatBorder" },
-          { "╘", "FloatBorder" },
-          { "│", "FloatBorder" },
-        },
-        auto_focus = true,
+      lazy = false,
+      priority = 1000,
+      opts = {
+        style = "night",
+        comments = { italic = true },
+        keywords = { italic = true },
       },
     },
-    }
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.go" },
+    { import = "astrocommunity.completion.copilot-lua" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.pack.cpp" },
   },
+  lsp = {
+    config = {
+      clangd = {
+        capabilities = {
+          offsetEncoding = "utf-8",
+        }
+      }
+    },
   },
   polish = function()
-    vim.cmd(":set mouse = ")
+  -- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+  -- vim.cmd("hi CursorColumn cterm=NONE ctermbg=NONE ctermfg=NONE")
+  -- vim.cmd("hi CursorLine cterm=NONE ctermbg=NONE ctermfg=NONE")
+  -- vim.cmd("hi CursorLineNr cterm=NONE ctermbg=NONE ctermbg=NONE")
+  -- vim.cmd("hi clear LineNr")
+  -- vim.cmd("hi clear SignColumn")
+  -- vim.cmd("hi clear StatusLine")
   end,
 }
