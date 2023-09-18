a46c364cf2ba8acfe57c0800add0070e34b47a7f
Parker <aparkerdf@protonmail.com>
2023-09-18 18:52:28 -0300

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,14 @@
 alias ls='lsd'
 alias nv='nvim'
+alias doc_server='cd ~/.cargo-target/doc && python3 -m http.server 3494'
 set fish_greeting
 if status is-interactive
-    neofetch
-    # Commands to run in interactive sessions can go here
+    # Check if we are not in tmux
+    if test -z "$TMUX"
+        neofetch
+    end
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -1,60 +1,71 @@
 return {
-  colorscheme = "tokyonight",
+  colorscheme = "catppuccin-mocha",
   -- the init key will apply to all colorschemes, this sets transparency
-  highlights = {
-    -- apply highlight group to all colorschemes (include the default_theme)
-    init = {
-      -- set the transparency for all of these highlight groups
-      Normal = { bg = "NONE", ctermbg = "NONE" },
-      NormalNC = { bg = "NONE", ctermbg = "NONE" },
-      CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-      CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-      CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-      LineNr = {},
-      SignColumn = {},
-      StatusLine = {},
-      NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
-      NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
-    },
-  },
-
+  -- highlights = {
+  --   -- apply highlight group to all colorschemes (include the default_theme)
+  --   init = {
+  --     -- set the transparency for all of these highlight groups
+  --     Normal = { bg = "NONE", ctermbg = "NONE" },
+  --     NormalNC = { bg = "NONE", ctermbg = "NONE" },
+  --     CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  --     CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  --     CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  --     LineNr = {},
+  --     SignColumn = {},
+  --     StatusLine = {},
+  --     NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+  --     NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+  --   },
+  -- },
 
   plugins = {
 
     "AstroNvim/astrocommunity",
     {
-      "folke/tokyonight.nvim",
+      "catppuccin/nvim",
       lazy = false,
       priority = 1000,
-      opts = {
-        style = "night",
-        comments = { italic = true },
-        keywords = { italic = true },
-      },
     },
     { import = "astrocommunity.pack.rust" },
     { import = "astrocommunity.pack.go" },
     { import = "astrocommunity.completion.copilot-lua" },
-    { import = "astrocommunity.pack.dart" },
     { import = "astrocommunity.pack.python" },
-    { import = "astrocommunity.pack.cpp" },
+    { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.pack.vue" },
   },
   lsp = {
     config = {
       clangd = {
         capabilities = {
           offsetEncoding = "utf-8",
-        }
-      }
+        },
+        cmd = {
+          "clangd",
+          "--enable-config",
+          "--clang-tidy",
+          "--query-driver=/usr/bin/riscv64-elf-gcc",
+          "--compile-commands-dir=/home/parker/github/xv6-riscv/compile_commands.json",
+        },
+      },
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            checkOnSave = {
+              allTargets = false,
+            },
+          },
+        },
+      },
     },
   },
   polish = function()
-  -- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-  -- vim.cmd("hi CursorColumn cterm=NONE ctermbg=NONE ctermfg=NONE")
-  -- vim.cmd("hi CursorLine cterm=NONE ctermbg=NONE ctermfg=NONE")
-  -- vim.cmd("hi CursorLineNr cterm=NONE ctermbg=NONE ctermbg=NONE")
-  -- vim.cmd("hi clear LineNr")
-  -- vim.cmd("hi clear SignColumn")
-  -- vim.cmd("hi clear StatusLine")
+    -- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+    vim.cmd("set mouse=")
+    -- vim.cmd("hi CursorColumn cterm=NONE ctermbg=NONE ctermfg=NONE")
+    -- vim.cmd("hi CursorLine cterm=NONE ctermbg=NONE ctermfg=NONE")
+    -- vim.cmd("hi CursorLineNr cterm=NONE ctermbg=NONE ctermbg=NONE")
+    -- vim.cmd("hi clear LineNr")
+    -- vim.cmd("hi clear SignColumn")
+    -- vim.cmd("hi clear StatusLine")
   end,
 }
