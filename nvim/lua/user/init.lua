76f50143fc3ddb44e4a51d0747d708392a2e3570
Parker <aparkerdf@protonmail.com>
2023-10-10 12:51:51 -0300

    Update

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -28,6 +28,7 @@ return {
     },
     { import = "astrocommunity.pack.rust" },
     { import = "astrocommunity.pack.go" },
+    { import = "astrocommunity.pack.java" },
     { import = "astrocommunity.completion.copilot-lua" },
     { import = "astrocommunity.pack.python" },
     { import = "astrocommunity.pack.typescript" },
