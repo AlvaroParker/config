b8a4e2a871f71f1787990621234a4a6a549f0323
Parker <aparkerdf@protonmail.com>
2023-11-02 17:05:47 -0300

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,9 @@
 alias ls='lsd'
 alias nv='nvim'
+alias tree='exa --icons=always --tree'
 alias doc_server='cd ~/.cargo-target/doc && python3 -m http.server 3494'
 set fish_greeting
+set JAVA_HOME /usr/lib/jvm/default
 if status is-interactive
     # Check if we are not in tmux
     if test -z "$TMUX"
