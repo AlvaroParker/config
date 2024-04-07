23792bd245bf3a21732bc24d060c1a2bdb53686c
Parker <aparkerdf@protonmail.com>
2024-04-07 09:24:46 -0400

    Update & fix waybar

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -1,22 +1,22 @@
 return {
   colorscheme = "catppuccin-mocha",
   -- the init key will apply to all colorschemes, this sets transparency
-  -- highlights = {
-  --   -- apply highlight group to all colorschemes (include the default_theme)
-  --   init = {
-  --     -- set the transparency for all of these highlight groups
-  --     Normal = { bg = "NONE", ctermbg = "NONE" },
-  --     NormalNC = { bg = "NONE", ctermbg = "NONE" },
-  --     CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-  --     CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-  --     CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-  --     LineNr = {},
-  --     SignColumn = {},
-  --     StatusLine = {},
-  --     NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
-  --     NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
-  --   },
-  -- },
+  highlights = {
+    -- apply highlight group to all colorschemes (include the default_theme)
+    init = {
+      -- set the transparency for all of these highlight groups
+      Normal = { bg = "NONE", ctermbg = "NONE" },
+      NormalNC = { bg = "NONE", ctermbg = "NONE" },
+      CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      LineNr = {},
+      SignColumn = {},
+      StatusLine = {bg = "NONE", ctermbg = "NONE"},
+      NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+      NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+    },
+  },
 
   plugins = {
 
