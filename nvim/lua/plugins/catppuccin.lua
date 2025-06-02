d5671b59ea750d7a36c7d67ef996ddd0eaaaf5bd
Alvaro Parker <aparkerdf@protonmail.com>
2025-06-01 23:09:40 -0400

    Update

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,6 +1,13 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
+  "catppuccin/nvim",
+  opts = {
+    transparent_background = true, -- catppuccin has this option built-in
+    integrations = {
+      -- enable integrations as you like
+    },
   },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd("colorscheme catppuccin")
+  end,
 }
--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,2 +1,3 @@
 vim.cmd("set mouse=")
 vim.cmd("set wrap")
+
