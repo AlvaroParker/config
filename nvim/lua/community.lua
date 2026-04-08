e09a35a15f7b42bb0236512296e217443d6fc617
Alvaro Parker <aparkerdf@protonmail.com>
2026-04-08 13:16:59 -0400

    Update nvim

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -1,12 +1,10 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
 ---@type LazySpec
 return {
     "AstroNvim/astrocommunity",
-    { import = "astrocommunity.pack.rust" },
-    { import = "astrocommunity.pack.go" },
-    { import = "astrocommunity.pack.java" },
-    { import = "astrocommunity.completion.copilot-lua" },
-    { import = "astrocommunity.pack.python" },
-    { import = "astrocommunity.pack.cpp" },
-    { import = "astrocommunity.pack.typescript-all-in-one" },
     { import = "astrocommunity.color.transparent-nvim" },
+    -- import/override with your plugins folder
 }
--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,3 +1,6 @@
 vim.cmd("set mouse=")
 vim.cmd("set wrap")
 
+-- This will run last in the setup process.
+-- This is just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
